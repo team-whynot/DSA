8575279ea99b72683d1ab3ceff4608987f97dfb4
rahulmistry33 <priyarahul0307@gmail.com>
2021-10-29 20:50:48 +0530

    Added two sum problem

---
+++ TwoSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int arr[] = new int[2];
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(target-nums[i])){
+                arr[0] = hm.get(target-nums[i]);
+                arr[1] = i;
+                return arr;
+            }
+            else{
+                hm.put(nums[i],i);
+            }
+        }
+        return arr;
+    }
+}
